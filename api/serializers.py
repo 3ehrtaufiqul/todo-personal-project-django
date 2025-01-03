5fb84a174f5b22bbd4ccdd04c54067ade4dfc208
Muhammad Taufiqul Mawarid <taufiqul@3ecpa.com.sg>
2025-01-03 16:55:55 +0700

    changed username to user id in json response to optimize django sql query
    previously django launch sql query for every todo list in order to find the username of that todo list owner, even if the last todo has the same owner as the next one.

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Todo
