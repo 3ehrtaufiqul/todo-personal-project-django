fe44532f171b8e8b2cc4dd1e79f8f59b59d0efb4
Muhammad Taufiqul Mawarid <taufiqul@3ecpa.com.sg>
2025-01-03 15:19:03 +0700

    basic django auth

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Todo
-        fields = '__all__'
+        fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import generics, permissions, status
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import generics
 
 from api.serializers import TodoSerializer
 
@@ -14,11 +13,17 @@ from .models import Todo
 
 class TodoList(generics.ListCreateAPIView):
     "List all todos or create a new todo."
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
+    def perform_create(self, serializer):
+        # Use perform_create hook to associate the todo with the user that created it before saving it to the database.
+        serializer.save(user=self.request.user)
+
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     "Retrieve, update, or delete a todo."
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     lookup_field = 'id'
