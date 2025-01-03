8cce4a40da6e6ee0564bc69abd1327dc969fc1c8
Muhammad Taufiqul Mawarid <taufiqul@3ecpa.com.sg>
2025-01-03 09:07:33 +0700

    implement todo models and serializer

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    note = models.TextField(default="")
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
