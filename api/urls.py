8abfe09902dc4e46f63697008b55dcd92556e2c9
Muhammad Taufiqul Mawarid <taufiqul@3ecpa.com.sg>
2025-01-03 09:32:18 +0700

    rest framework tutorial 1

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.todo_list, name='todo_list'),
+    path('<int:id>/', views.todo_detail, name='todo_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from api.serializers import TodoSerializer
+from .models import Todo
 
 def index(request):
     return HttpResponse("Hello, world. You're at the todo index.")
+
+@csrf_exempt
+def todo_list(request):
+    # get all todos
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def todo_detail(request, id):
+    # get, update, or delete single todo
+
+    try:
+        todo = Todo.objects.get(pk=id)
+    except Todo.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TodoSerializer(todo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    if request.method == 'DELETE':
+        todo.delete()
+        return HttpResponse(status=204)
